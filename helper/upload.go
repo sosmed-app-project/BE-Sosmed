5f30b6a336c4879bd5d1ed794c21a64ef14c0e35
Muhammad Ilham <ilham9d27@gmail.com>
2023-09-22 08:39:56 +0700

    update deploy

--- helper/upload.go
+++ helper/upload.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"log"
 	"mime/multipart"
-	"os"
 	"time"
 
 	"cloud.google.com/go/storage"
@@ -27,7 +26,7 @@ type ClientUploader struct {
 var Uploader *ClientUploader
 
 func init() {
-	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "keys.json") // FILL IN WITH YOUR FILE PATH
+	// os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "keys.json") // FILL IN WITH YOUR FILE PATH
 	client, err := storage.NewClient(context.Background())
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
