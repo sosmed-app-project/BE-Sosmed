f29e78ce7a734b7c6129efeb2fbb71f6e5f90370
kotutshi <surercreature53@gmail.com>
2023-12-07 14:51:51 +0700

    menambahkan handler

--- feature/like/entities.go
+++ feature/like/entities.go
@@ -1,1 +1,13 @@
 package like
+
+import "time"
+
+// Like model
+type Like struct {
+	ID        uint      `json:"id"`
+	UserID    uint      `json:"user_id"`
+	PostID    uint      `json:"post_id"`
+	CountLike bool      `json:"count_like"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
--- feature/posting/entities.go
+++ feature/posting/entities.go
@@ -1,3 +1,17 @@
 package posting
 
+import (
+	"time"
+)
 
+// User model
+type User struct {
+	ID          uint      `json:"id"`
+	Username    string    `json:"username"`
+	DateOfBirth time.Time `json:"date_of_birth"`
+	Email       string    `json:"email"`
+	PhoneNumber string    `json:"phone_number"`
+	Password    string    `json:"password"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
