9d1717df5fbfef535dda5d64ffd864360dac73eb
Julius Siregar <juliussiregar@MacBook-Air-Julius.local>
2023-12-07 19:18:59 +0700

    update connect database

--- feature/users/handler/handler.go
+++ feature/users/handler/handler.go
@@ -1,26 +1,2 @@
-package main
+package handler
 
-import (
-	"net/http"
-	"strconv"
-
-	"github.com/gin-gonic/gin"
-)
-
-// GetUserHandler mengambil data pengguna berdasarkan ID
-func GetUserHandler(c *gin.Context) {
-	userID, err := strconv.Atoi(c.Param("user_id"))
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
-		return
-	}
-
-	// Panggil fungsi untuk mendapatkan data pengguna dari database
-	user, err := getUserFromDatabase(userID)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get user"})
-		return
-	}
-
-	c.JSON(http.StatusOK, user)
-}
